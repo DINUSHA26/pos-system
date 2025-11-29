b2e8ee788865b54575736503c0e9c9f1096b9798
Manidu Dinusha <115367541+DINUSHA26@users.noreply.github.com>
2025-11-30 00:01:54 +0530

    model setup final

---
+++ src/main/java/com/kmd_project/pos_system/model/User.java
@@ -0,0 +1,60 @@
+package com.kmd_project.pos_system.model;
+
+
+import com.cdz.domain.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "Email should be valid")
+    private String email;
+
+
+    @ManyToOne
+    private Store store;
+
+    @ManyToOne
+    private Branch branch;
+
+    private String phone;
+
+    @Column(length = 50,nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime lastLogin;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
